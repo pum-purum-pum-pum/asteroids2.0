633fdb2b5ea98d17afcb4a458d65933780cb15a9
Vlad <vladzhukovtirko@gmail.com>
2019-12-06 15:10:54 +0300

    add space key for upgrade and done buttons

--- src/systems/upgrade_control.rs
+++ src/systems/upgrade_control.rs
@@ -16,16 +16,22 @@ impl<'a> System<'a> for UpgradeControlSystem {
         Write<'a, UI>,
 	    Write<'a, SpawnedUpgrades>,
         WriteExpect<'a, UIState>,
+        Read<'a, AvaliableUpgrades>,
+	    WriteExpect<'a, Vec<UpgradeType>>,
+	    Write<'a, AppState>,
 	);
 
     fn run(&mut self, data: Self::SystemData) {
     	let (
     		mut key_codes,
     		mut ui,
-    		spawned_upgrades,
-    		mut ui_state
+    		mut spawned_upgrades,
+    		mut ui_state,
+    		avaliable_upgrades,
+    		mut upgrade_types,
+    		mut app_state
     	) = data;
-    	let upgrades = spawned_upgrades.last();
+    	let upgrades = spawned_upgrades.last().map(|x| x.clone());
         let widget_ids = [Widgets::Upgrade1, Widgets::Upgrade2];
         let widget_selector = Widgets::UpgradeSelector as usize;
     	swap(&mut self.prev_keys, &mut self.new_keys);
@@ -35,10 +41,10 @@ impl<'a> System<'a> for UpgradeControlSystem {
     		// do something here
     	}
     	let new_pressed = &self.new_keys - &self.prev_keys;
-    	if let Some(upgrades) = upgrades {
-	    	for key in new_pressed.iter() {
-	    		match key {
-	                Keycode::Left | Keycode::Right => {
+    	for key in new_pressed.iter() {
+    		match key {
+                Keycode::Left | Keycode::Right => {
+			    	if let Some(upgrades) = upgrades {
 	                	if ui.selected(widget_selector, widget_ids[0] as usize) 
 	                			|| ui.free_selector(widget_selector) {
 							ui_state.choosed_upgrade = Some(upgrades[1]);
@@ -47,16 +53,22 @@ impl<'a> System<'a> for UpgradeControlSystem {
 							ui_state.choosed_upgrade = Some(upgrades[0]);
 	                		ui.select(widget_selector, widget_ids[0] as usize)
 	                	}
-	                }
-	                Keycode::Space => {
-	                	
-	                }
-	                // Keycode::Right => {
-	                // 	dbg!("sd");
-	                // }
-	                _ => ()
-	    		}
-	    	}    		
-    	}
+			    	}
+                }
+                Keycode::Space => {
+    	            if let Some(upgrade) = ui_state.choosed_upgrade {
+	                    ui_state.choosed_upgrade = None;
+	                    spawned_upgrades.pop();
+			            upgrade_types.push(avaliable_upgrades[upgrade].upgrade_type);
+    	            } else {
+        	            *app_state = AppState::Play(PlayState::Action);
+    	            }
+                }
+                // Keycode::Right => {
+                // 	dbg!("sd");
+                // }
+                _ => ()
+    		}
+    	}    		
     }
 }
